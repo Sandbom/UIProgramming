d56e804cfc455d2f94cc8d531c761786d9554797
Alex Wahlandt <alex.wahlandt@gmail.com>
2018-03-01 15:51:55 +0100

    added a translationspage, and the corresponding code to fetch the translations to custerBeerInfo

---
+++ BlowTheFogHorn/Database/Dutchman Files/translations.js
@@ -0,0 +1,37 @@
+var swedish = {
+    "customerBeerInfo": [
+        {
+            "origin" : "Ursprung: ",
+            "provider" : "Leverantör: ",
+            "nr" : "Nr: ",
+            "classOfGoods" : "Varugrupp: ",
+            "alcoholPercentage" : "Alkoholhalt: ",
+            "packaging" : "förpackning: "
+
+        }
+        
+
+    ]
+
+}
+
+
+var english = {
+    "customerBeerInfo" : [
+        {
+            "origin" : "Origin: ",
+            "provider" : "Provider: ",
+            "nr" : "Nr: ",
+            "classOfGoods" : "Class of goods: ",
+            "alcoholPercentage" : "Alcohol percentage: ",
+            "packaging" : "packaging: "
+
+        }
+
+
+
+    ]
+
+
+
+}
