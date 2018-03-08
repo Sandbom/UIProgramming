750619ccf76c7d68da0cec4eb7d3719a8fb764fb
Alex Wahlandt <alex.wahlandt@gmail.com>
2018-03-08 18:43:54 +0100

    Tried to implement the cart, some problems tho

--- BlowTheFogHorn/Scripts/cart.js
+++ BlowTheFogHorn/Scripts/cart.js
@@ -1,3 +1,7 @@
+$.getScript("../Database/Dutchman Files/beverages.js", function() {
+    alert("Script loaded but not necessarily executed.");
+});
+
 /*
         We need to create cart once a user logs into the system.
          */
@@ -9,14 +13,27 @@ function createCart() {
 
 function displayCart(){
     var cart = JSON.parse(localStorage.getItem("cart"));
-
+    var beer;
     console.log("Display cart ");
     console.log(cart);
 
-    for (var i = 0;i < cart.length;i++){
+    for (var i = 0; i < cart.length;i++){
+        beer = findBeer(cart[i]);
+        alert(beer);
+    }
 
+}
+
+function findBeer(beerName) {
+    var info = [];
+
+    for (var i = 0;DB2.spirits.length;i++){
+        if (DB2.spirits[i].namn ===beerName){
+            info.push(DB2.spirits[i]);
+        }
 
     }
+    return info;
 }
 
 
