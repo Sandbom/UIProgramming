e071e6c07ac45858b31195c84a7a1fd0c189166f
Sandbom <simon.sandbom@gmail.com>
2018-03-16 12:26:25 +0100

    added proper stack adds/pops to undo

--- BlowTheFogHorn/Scripts/undoredo.js
+++ BlowTheFogHorn/Scripts/undoredo.js
@@ -7,20 +7,23 @@ var current = -1;
 // This function is triggered when pressing the undo button and it removes the last child
 // from the div with id "cartview"
 function undoclick(){
-	var list = document.getElementById("cartview");
-	list.removeChild(list.lastChild);
-	current--;
+	if(current>= 0){
+		var list = document.getElementById("cartview");
+		list.removeChild(list.lastChild);
+		current--;
+		addtostack(list.lastChild);
+	}
 }
 
 //This function is triggered when pressing the redo button and it pops the most recent item from the stack
 // and re-adds it to the div element with id "cartview"
 function redoclick(){
-	if (current >=0){
+	//if ((stack.length)>0)){ // I dont know why this does not work and it is kind of needed
 		itemtoredo = popfromstack();
 		var cartlist = document.getElementById("cartview");
 		cartlist.appendChild(itemtoredo);
 		current++;
-	}
+//	}
 }
 
 // Here we push to the stack so that we know what to extract later when extracting,
@@ -33,7 +36,7 @@ function addtostack(object){
 // This function returns the most recently added element from the stack 
 // assuming there has been a drag & drop in the first place
 function popfromstack(){
-		//popped = stack.pop() //not sure if this is correct, I feel that we want to pop the item off the stack at some point
-		popped = stack[current];
+		popped = stack.pop() //not sure if this is correct, I feel that we want to pop the item off the stack at some point
+		//popped = stack[current];
 		return popped;
 }
