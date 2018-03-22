c0a6fcadddbe1cf14b00b852e4288d3dbdae0335
Sandbom <simon.sandbom@gmail.com>
2018-03-22 16:38:34 +0100

    a start

--- BlowTheFogHorn/Scripts/cart.js
+++ BlowTheFogHorn/Scripts/cart.js
@@ -1,30 +1,28 @@
-$.getScript("../Database/Dutchman Files/Beverages.js", function() {
-});
-
-
-function displayCart(){
+$(function (){
 
     var cart = JSON.parse(sessionStorage.getItem("cart"));
-    var beer;
-
-    for (var i = 0; i < cart.length;i++){
-        beer = findBeer(cart[i]);
-
-    }
-}
+    var count = 0;
+        for (var i = 0; i < cart.length;i++){
+            var beer = findBeer(cart[i]);
+            var myTable = document.getElementById('tableid');
+            myTable.rows[i+1].cells[1].innerHTML = beer[i].namn;
+            myTable.rows[i+1].cells[3].innerHTML = beer[i].prisinklmoms
+        }
+});
 
 function findBeer(beerName) {
+
     var info = [];
+    var count = 1;
 
     for (var i = 0;i < DB2.spirits.length;i++){
         if (DB2.spirits[i].namn === beerName){
-            alert("träff");
             info.push(DB2.spirits[i]);
+            alert("This is being added to the list: " + count + " " + DB2.spirits[i].namn);
+            count++;
         }
 
     }
     return info;
 }
-
-
 
