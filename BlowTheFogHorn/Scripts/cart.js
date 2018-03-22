6276b80bd4d892c6418ada2bea50a5f8487de6b6
joseph13g <joseph13g@gmail.com>
2018-03-22 20:10:34 +0100

    fixed localstorage in cart

--- BlowTheFogHorn/Scripts/cart.js
+++ BlowTheFogHorn/Scripts/cart.js
@@ -1,28 +1,42 @@
-$(function (){
+var info = [];
+$(document).ready((function (){
 
     var cart = JSON.parse(sessionStorage.getItem("cart"));
     var count = 0;
+    var myTable = document.getElementById('tableid');
+    var beers = [] ;
         for (var i = 0; i < cart.length;i++){
+           // alert(cart[i]);
             var beer = findBeer(cart[i]);
-            var myTable = document.getElementById('tableid');
-            myTable.rows[i+1].cells[1].innerHTML = beer[i].namn;
-            myTable.rows[i+1].cells[3].innerHTML = beer[i].prisinklmoms
-        }
-});
+            beers[i] = beer;
+            alert("To show: "+beer);
 
+        }
+   // alert(beers[0]);
+   // alert(beers);
+}));
+
+//myTable.rows[i+1].cells[1].innerHTML = beer[i].namn;
+//myTable.rows[i+1].cells[3].innerHTML = beer[i].prisinklmoms
 function findBeer(beerName) {
 
-    var info = [];
+
     var count = 1;
+    alert("Result: "+DB2.spirits.length);
+    alert("Name: "+beerName);
 
     for (var i = 0;i < DB2.spirits.length;i++){
-        if (DB2.spirits[i].namn === beerName){
-            info.push(DB2.spirits[i]);
-            alert("This is being added to the list: " + count + " " + DB2.spirits[i].namn);
-            count++;
+        console.log(DB2.spirits[i].namn);
+        if  (DB2.spirits[i].namn ===beerName.trim()){
+
+            alert("Result: "+DB2.spirits[i]);
+            return(DB2.spirits[i].namn);
+           // alert("This is being added to the list: " + count + " " + DB2.spirits[i].namn);
+
         }
+        return "not found";
 
     }
-    return info;
+
 }
 
