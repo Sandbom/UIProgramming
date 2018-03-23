213bfc80c0553a360b2a065fae74ddc2cc3d2b76
Sandbom <simon.sandbom@gmail.com>
2018-03-23 01:56:32 +0100

    fully working cart, albeit ugly

--- BlowTheFogHorn/Scripts/cart.js
+++ BlowTheFogHorn/Scripts/cart.js
@@ -1,42 +1,52 @@
-var info = [];
 $(document).ready((function (){
 
     var cart = JSON.parse(sessionStorage.getItem("cart"));
     var count = 0;
     var myTable = document.getElementById('tableid');
-    var beers = [] ;
-        for (var i = 0; i < cart.length;i++){
-           // alert(cart[i]);
-            var beer = findBeer(cart[i]);
-            beers[i] = beer;
-            alert("To show: "+beer);
+    var n = 0;
 
+    /* Adds the total sum from the previously stored variable in sessionstorage*/
+    var totalsumvar = sessionStorage.getItem("totalsum");
+    $("#totalamount").text("Totalsumma: " + totalsumvar + " kr.");
+
+        for (var i = 0; i < cart.length; i++){
+            var beer = findBeer(cart[i]);
+            var beerprice = findBeerprice(cart[i]);
+           //var row = myTable.insertRow(myTable.rows.length);
+           //var cell1 = row.insertCell(0);
+           //cell1.innerHTML = beer;
+           myTable.rows[i+1].cells[0].innerHTML = beer;
+           myTable.rows[i+1].cells[1].innerHTML = beerprice;
         }
-   // alert(beers[0]);
-   // alert(beers);
+
 }));
 
-//myTable.rows[i+1].cells[1].innerHTML = beer[i].namn;
-//myTable.rows[i+1].cells[3].innerHTML = beer[i].prisinklmoms
+
 function findBeer(beerName) {
-
-
-    var count = 1;
-    alert("Result: "+DB2.spirits.length);
-    alert("Name: "+beerName);
-
-    for (var i = 0;i < DB2.spirits.length;i++){
-        console.log(DB2.spirits[i].namn);
-        if  (DB2.spirits[i].namn ===beerName.trim()){
-
-            alert("Result: "+DB2.spirits[i]);
-            return(DB2.spirits[i].namn);
-           // alert("This is being added to the list: " + count + " " + DB2.spirits[i].namn);
-
+    var info = [];
+    for (var i = 0; i < DB2.spirits.length; i++){
+        if(DB2.spirits[i].namn === beerName){
+            info.push(DB2.spirits[i].namn);
+            break;
         }
-        return "not found";
 
     }
+    return info;
 
 }
+
+/* Does the same as findBeer except it pushes to price of a beer to a list, can be done better than this*/
+function findBeerprice(beerName) {
+    var beerprices = []
+    for (var i = 0; i < DB2.spirits.length; i++){
+        if(DB2.spirits[i].namn === beerName){
+            beerprices.push(DB2.spirits[i].prisinklmoms);
+            break;
+        }
+
+    }
+    return beerprices;
+
+}
+
 
