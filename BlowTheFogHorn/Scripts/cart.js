9dbc0d14d803fa5a3f484886d833c1924667bc79
joseph13g <joseph13g@gmail.com>
2018-03-23 14:16:43 +0100

    Fixed cart

--- BlowTheFogHorn/Scripts/cart.js
+++ BlowTheFogHorn/Scripts/cart.js
@@ -3,6 +3,8 @@ $(document).ready((function (){
     var cart = JSON.parse(sessionStorage.getItem("cart"));
     var count = 0;
     var myTable = document.getElementById('tableid');
+    var toAppendName = [];
+    var toAppendPrice = [];
     var n = 0;
 
     /* Adds the total sum from the previously stored variable in sessionstorage*/
@@ -14,9 +16,27 @@ $(document).ready((function (){
            //var row = myTable.insertRow(myTable.rows.length);
            //var cell1 = row.insertCell(0);
            //cell1.innerHTML = beer;
-           myTable.rows[i+1].cells[0].innerHTML = beer;
-           myTable.rows[i+1].cells[1].innerHTML = beerprice;
+           //myTable.rows[i+1].cells[0].innerHTML = beer;
+          // myTable.rows[i+1].cells[1].innerHTML = beerprice;
+            toAppendName[i] = beer;
+            toAppendPrice[i] = beerprice;
+
         }
+    for (var j = 0; j < toAppendName.length; j++){
+        var newRow = myTable.insertRow(myTable.rows.length);
+// Insert a cell in the row at index 0
+        var nameCell = newRow.insertCell(0);
+        var name = document.createTextNode(toAppendName[j]);
+        nameCell.appendChild(name);
+
+// Insert a cell in the row at index 2
+        var priceCell = newRow.insertCell(1);
+        var price = document.createTextNode(toAppendPrice[j]);
+        priceCell.appendChild(price);
+
+    }
+
+
 
 }));
 
@@ -40,6 +60,7 @@ function findBeerprice(beerName) {
     for (var i = 0; i < DB2.spirits.length; i++){
         if(DB2.spirits[i].namn === beerName){
             beerprices.push(DB2.spirits[i].prisinklmoms);
+            break;
 
           }
         }
